5cf2e0ce4a3b31c8c45cc8011960b8b638af4765
IFCE <IFCE@bpid.local>
2017-07-13 18:04:05 -0300

    Fix Bayes Theorem.

--- Probability.py
+++ Probability.py
@@ -1,15 +1,30 @@
 class Probability:
+    #
+    # P(a) = n(a)/n(s)
+    #
     def of_A(self, numberOfCases, numberOfPossibilities):
         return numberOfCases/numberOfPossibilities
 
-    def intersection_Of_A_and_B(self, numberOfIntersectionAAndB, probabilityOfB):
-        return numberOfIntersectionAAndB/probabilityOfB
-
-    def intersection_independent_Of_A_and_B(self, probabilityOfA, probabilityOfB):
+    #
+    # P(a,b) = P(a)*P(b)
+    #
+    def intersection_Of_A_and_B(self, probabilityOfA, probabilityOfB):
         return probabilityOfB * probabilityOfA
 
+    #
+    # P(a|b) = P(a,b)/P(b)
+    #
     def of_A_Given_B(self, probabilityOfA, probabilityOfB):
         return Probability.intersection_Of_A_and_B(self, probabilityOfA, probabilityOfB)/probabilityOfB
 
-    def bayes_Theorem_with_A_given_B(self, probabilityOfA, probabilityOfB, probabilityOfBGivenA):
-        return ( probabilityOfBGivenA * probabilityOfA)/probabilityOfB
+    #
+    # P(a|X) = P(a|x1, x2, x3,..., xi)
+    # P(a|X) = p(a) * p(x1|a) * p(x2|a) * p(xi|a)
+    #
+    def bayes_Theorem_with_A_given_X(self, probabilityOfA, probabilitiesOfX):
+        probability = 1.0
+
+        for i in probabilitiesOfX:
+            probability = probability * Probability.of_A_Given_B(probabilitiesOfX[i], probabilityOfA)
+
+        return probabilityOfA * probability
